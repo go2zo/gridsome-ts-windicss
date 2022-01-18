8cb42c399c33799b8fb53ec9bab7ad4dda96b72a
Jiho Kim <go2zo.ooo@gmail.com>
2022-01-18 11:26:48 +0900

    chore(dev): add svg to component plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,9 +4,36 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+const nodeExternals = require('webpack-node-externals')
+
 module.exports = {
   siteName: 'WindicssStarter',
   siteDescription: 'Gridsome starter with Windicss',
+  chainWebpack: (config, { isServer }) => {
+    config.module.rules.delete('svg')
+    config.module
+      .rule('svg')
+      .test(/\.svg$/)
+      .use('vue')
+      .loader('vue-loader')
+      .end()
+      .use('svg-to-vue-component')
+      .loader('svg-to-vue-component/loader')
+
+    if (isServer) {
+      config.externals(
+        nodeExternals({
+          whitelist: [
+            /\.css$/,
+            /\?vue&type=style/,
+            /vue-instantsearch/,
+            /instantsearch.js/,
+            /typeface-league-spartan/,
+          ],
+        })
+      )
+    }
+  },
   templates: {
     BlogPost: '/blog/:year/:month/:day/:slug',
   },
--- src/components/theme/app/AppHeader.vue
+++ src/components/theme/app/AppHeader.vue
@@ -1,15 +1,20 @@
 <template>
-  <header
-    class="sticky z-20 top-0 bg-transparent border-b flex-nowrap transition-colors duration-300 ease-linear"
-  >
-    <div class="p-0.5 text-center text-white text-base bg-gradient-to-r from-green-500 via-blue-400 to-red-500" />
+  <header class="sticky z-20 top-0 bg-transparent border-b flex-nowrap">
     <div
-      class="container flex gap-32 px-0 py-6 min-h-14"
-    >
+      class="p-0.5 text-center text-white text-base bg-gradient-to-r from-green-500 via-blue-400 to-red-500"
+    />
+    <div class="container flex gap-32 px-6 py-0 min-h-14">
+      <div class="flex items-center flex-1 justify-center lg:justify-start">
+        <Logo class="h-8 mr-4 md:h-9 lg:mr-0" />
+      </div>
       <nav class="flex-nowrap flex items-center gap-4">
-        <g-link v-for="(link, index) in links" :key="index" class="pt-5px pb-10px text-current" :to="link.slug">{{
-          link.title
-        }}</g-link>
+        <g-link
+          v-for="(link, index) in links"
+          :key="index"
+          class="pt-5px pb-10px text-current"
+          :to="link.slug"
+          >{{ link.title }}</g-link
+        >
       </nav>
     </div>
   </header>
@@ -24,7 +29,12 @@ query {
 </static-query>
 
 <script>
+import Logo from '~/assets/images/gridsome-logo.svg'
+
 export default {
+  components: {
+    Logo,
+  },
   props: {
     links: {
       type: Array,
